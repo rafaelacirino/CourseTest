2d2d97638e7fff7144ef3cff4fcfa019b46806b3
Rafaela Cirino <rafaelaborbaf@gmail.com>
2022-12-01 18:24:58 -0300

    Interface UserRepository findById

---
+++ src/main/java/com/rafaela/api/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafaela.api.repositories;
+
+import com.rafaela.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
