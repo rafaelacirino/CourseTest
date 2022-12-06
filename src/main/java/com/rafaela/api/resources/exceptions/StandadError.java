e7a652e02891e30037ee8559199d9940514e2a47
Rafaela Cirino <rafaelaborbaf@gmail.com>
2022-12-05 22:05:43 -0300

    tratamento de exceção para objeto não encontrado

---
+++ src/main/java/com/rafaela/api/resources/exceptions/StandadError.java
@@ -0,0 +1,19 @@
+package com.rafaela.api.resources.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class StandadError {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String path;
+
+}
--- src/main/java/com/rafaela/api/services/impl/UserServiceImpl.java
+++ src/main/java/com/rafaela/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.rafaela.api.services.impl;
 import com.rafaela.api.domain.User;
 import com.rafaela.api.repositories.UserRepository;
 import com.rafaela.api.services.UserService;
+import com.rafaela.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findById(Integer id) {
         Optional<User> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("objeto não encontrado"));
     }
 }
