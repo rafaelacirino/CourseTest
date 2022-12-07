0a6cff540d4ddc1bd363beb5fb2c6908d2ef97ef
Rafaela Cirino <rafaelaborbaf@gmail.com>
2022-12-07 19:44:59 -0300

    teste para classe UserService método update com sucesso

--- src/test/java/com/rafaela/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/rafaela/api/services/impl/UserServiceImplTest.java
@@ -121,7 +121,16 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
