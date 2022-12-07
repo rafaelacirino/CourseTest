5383b65125ec989caf7d824aa72034befeb19404
Rafaela Cirino <rafaelaborbaf@gmail.com>
2022-12-07 19:29:28 -0300

    Revert "teste para classe UserService método create"
    This reverts commit 0ef93e241c332a2141a71c123afbfe5894527f61.

--- src/test/java/com/rafaela/api/services/impl/UserServiceImplTest.java
+++ src/test/java/com/rafaela/api/services/impl/UserServiceImplTest.java
@@ -17,7 +17,6 @@ import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
@@ -95,17 +94,7 @@ class UserServiceImplTest {
     }
 
     @Test
-    void whenCreateThenReturnSuccess() {
-        when(repository.save(any())).thenReturn(user);
-
-        User response = service.create(userDTO);
-
-        assertNotNull(response);
-        assertEquals(User.class, response.getClass());
-        assertEquals(ID, response.getId());
-        assertEquals(NAME, response.getName());
-        assertEquals(EMAIL, response.getEmail());
-        assertEquals(PASSWORD, response.getPassword());
+    void create() {
     }
 
     @Test
